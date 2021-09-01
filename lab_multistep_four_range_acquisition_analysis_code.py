b0c455f7526f3925879eb54e4f178cdf47866883
Arda Ünal <43621489+ardaunal4@users.noreply.github.com>
2021-09-01 17:08:45 +0300

    Update lab_multistep_four_range_acquisition_analysis_code.py

--- lab_multistep_four_range_acquisition_analysis_code.py
+++ lab_multistep_four_range_acquisition_analysis_code.py
@@ -48,26 +48,26 @@ def statistical_analysis(range_name, voltage_column_name, step_number):
     
     for i in range(step_number):
         
-        filter1 = range_name['step'] == i + 1  # This 2 lines filters data according to step number
+        filter1 = range_name['step'] == i + 1                                                                               # This 2 lines filters data according to step number
         filtered_range = range_name[filter1]
-        volt_list = filtered_range[voltage_column_name] # This line takes voltage's values from filter data
+        volt_list = filtered_range[voltage_column_name]                                                                     # This line takes voltage's values from filter data
         current_level = float(filtered_range["current[mA]"].unique())
-        volt_values = [] # For every step it clears list of voltage's values
+        volt_values = []                                                                                                    # For every step it clears list of voltage's values
         
         for volt in volt_list:
             volt_values.append(float(volt))
                 
-        volt_values = volt_values[5::] # ignore first 5 values 
-        std_list.append('%.2E' % Decimal(std_calc(volt_values))) # Standart Deviation Calculation
-        current_list.append(current_level) # Add only one current's value 
-        step_list.append(i+1) # Step number
-        maximum.append('%.2E' % Decimal(max(volt_values))) # Max calcualiton
-        minimum.append('%.2E' % Decimal(min(volt_values))) # Min Calculation
+        volt_values = volt_values[5::]                                                                                      # ignore first 5 values 
+        std_list.append('%.2E' % Decimal(std_calc(volt_values)))                                                            # Standart Deviation Calculation
+        current_list.append(current_level)                                                                                  # Add only one current's value 
+        step_list.append(i+1)                                                                                               # Step number
+        maximum.append('%.2E' % Decimal(max(volt_values)))                                                                  # Max calcualiton
+        minimum.append('%.2E' % Decimal(min(volt_values)))                                                                  # Min Calculation
         step_mean.append('%.2E' % Decimal(np.mean(volt_values)))   
         pp.append('%.2E' % Decimal(max(volt_values) - min(volt_values))) 
         
-    statistic_list = [step_list, current_list, step_mean, std_list, maximum, minimum, pp] # Results in a list
-    return statistic_list # Function return list
+    statistic_list = [step_list, current_list, step_mean, std_list, maximum, minimum, pp]                                   # Results in a list
+    return statistic_list                                                                                                   # Function return list
 
 def histogram_of_steps(range_name, voltage_column_name, path, step_number):
     
@@ -87,15 +87,15 @@ def histogram_of_steps(range_name, voltage_column_name, path, step_number):
 
     for i in range(step_number):
         
-        filter1 = range_name['step'] == i + 1 # This 2 lines filters data according to step number
+        filter1 = range_name['step'] == i + 1                                                                              # This 2 lines filters data according to step number
         new_range = range_name[filter1]
-        volt_list = new_range[voltage_column_name] # This line takes voltage's values from filter data
-        volt_values = [] # For every step it clears list of voltage's values
+        volt_list = new_range[voltage_column_name]                                                                         # This line takes voltage's values from filter data
+        volt_values = []                                                                                                   # For every step it clears list of voltage's values
         
         for volt in volt_list:
             volt_values.append('%.2E' % Decimal(float(volt)))
 
-        volt_values = volt_values[5::] # ignore first 100 values
+        volt_values = volt_values[5::]                                                                                     # ignore first 100 values
         current_level = float(new_range["current[mA]"].unique())
         tittle = "Current Level of Histogram : {} [Amp]".format(current_level)
         plot_path = range_path + "\\hist_step" + str(i + 1) + ".jpg"
@@ -128,17 +128,17 @@ def fft_of_ranges_with_zero_current(path, range_name, voltage_column_name):
     else:
         print ("Successfully created the directory %s " % plots_path)
     
-    filter1 = range_name["step"] == 1  # This 2 lines filters data for zero current of the corresponding range
+    filter1 = range_name["step"] == 1                                                                                      # These 2 lines filter data for zero current of the corresponding range
     filtered_range = range_name[filter1]
     column_name = voltage_column_name
-    volt_list = filtered_range[column_name] # This line takes voltage's values from filter data
-    volt_values = [] # For every step it clears list of voltage's values
+    volt_list = filtered_range[column_name]                                                                                # This line takes voltage's values from filter data
+    volt_values = []                                                                                                       # For every step it clears list of voltage's values
     
     for volt in volt_list:
         volt_values.append(float(volt))
             
-    volt_values = volt_values[5::] # ignore first 5 values
-    signal = np.array(volt_values[0:1024]) # Use 2^10 values of the list
+    volt_values = volt_values[5::]                                                                                         # Ignore first 5 values
+    signal = np.array(volt_values[0:1024])                                                                                 # Use 2^10 values of the list
     sample_freq = fftfreq(signal.size)
     mask = sample_freq > 0 
     fft_signal = np.abs(fft(signal))
@@ -175,10 +175,10 @@ def step_by_step_plot(path, range_name, voltage_column_name, step_number):
         
     for i in range(step_number):
         
-        filter1 = range_name["step"] == i + 1  # This 2 lines filters data according to step number
+        filter1 = range_name["step"] == i + 1                                                                              # These 2 lines filter data according to step number
         filtered_range = range_name[filter1]
         column_name = voltage_column_name
-        volt_list = filtered_range[column_name] # This line takes voltage's values from filter data
+        volt_list = filtered_range[column_name]                                                                            # This line takes voltage's values from filter data
         times = filtered_range["time"]
         current_level = float(filtered_range["current[mA]"].unique())
         
@@ -188,12 +188,12 @@ def step_by_step_plot(path, range_name, voltage_column_name, step_number):
             date_time_str = time
             time_list.append(datetime.strptime(date_time_str, '%H:%M:%S'))
 
-        volt_values = [] # For every step it clears list of voltage's values
+        volt_values = []                                                                                                  # For every step it clears list of voltage's values
         for volt in volt_list:
             volt_values.append(float(volt))
                 
         time_list = time_list[5::]        
-        volt_values = volt_values[5::] # ignore first 5 values
+        volt_values = volt_values[5::]                                                                                    # ignore first 5 values
         plot_path = plots_path + "\\raw_plot_of_step" + str(i + 1) + ".jpg"
         
         plt.plot(time_list, volt_values)
@@ -217,7 +217,7 @@ def step_by_step_plot(path, range_name, voltage_column_name, step_number):
 def raw_plot(range_name, voltage_column_name, path):
 
     column_name = voltage_column_name
-    volt_list = range_name[column_name] # This line takes voltage's values from filter data
+    volt_list = range_name[column_name]                                                                                 # This line takes voltage's values from filter data
     times = range_name["time"]
 
     time_list = []
@@ -225,12 +225,12 @@ def raw_plot(range_name, voltage_column_name, path):
         date_time_str = time
         time_list.append(datetime.strptime(date_time_str, '%H:%M:%S'))
 
-    volt_values = [] # For every step it clears list of voltage's values
+    volt_values = []                                                                                                  # For every step it clears list of voltage's values
     for volt in volt_list:
         volt_values.append(float(volt))
         
     time_list = time_list[5::]        
-    volt_values = volt_values[5::] # ignore first 5 values
+    volt_values = volt_values[5::]                                                                                    # ignore first 5 values
     plot_path = path + "\\" + voltage_column_name + ".jpg"
     plt.figure(figsize = (10, 8))
     plt.plot(time_list, volt_values)
@@ -281,17 +281,17 @@ def configuration(csv_path):
     path_name, file_name = os.path.split(csv_path)    
     path = path_name + "\\analysis"
     
-    try:                        # This lines try to create a new directory 
+    try:                                                                                                              # This lines try to create a new directory 
         os.mkdir(path)
     except OSError:
         print ("Creation of the directory %s failed" % path)
     else:
         print ("Successfully created the directory %s " % path)
         
-    data = pd.read_csv(csv_path) # Read csv file as data frame and skip first 10 lines
+    data = pd.read_csv(csv_path)                                                                                      # Read CSV file as data frame and skip first 10 lines
     step_num = len(data["step"].unique())
 
-    # Seperate the range columns and step column from all data file
+                                                                                                                      # Seperate the range columns and step column from all data file
     range1 = data[["time", "step", "current[mA]", "RANGE1[V]"]]
     range2 = data[["time", "step", "current[mA]", "RANGE2[V]"]]
     range3 = data[["time", "step", "current[mA]", "RANGE3[V]"]]
@@ -345,13 +345,13 @@ def configuration(csv_path):
     average_plot(path, range4_results_dict["step"], range4_results_dict["average[V]"], "RANGE4")
     average_plot(path, Adc_24bit_dict["step"], Adc_24bit_dict["average[bin]"], "adc_24bit")
     
-    # Create Statistical Analysis csv file    
+                                                                                                                           # Create Statistical Analysis csv file    
     name_list = ["range4", "range3", "range2", "range1", "24bit"]
     features1 = ['step', 'current[mA]', 'average[V]', 'sd[V]', 'max[V]', 'min[V]', "peak-to-peak[V]"]
     features2 = ['step', 'current[mA]', 'average[bin]', 'sd[bin]', 'max[bin]', 'min[bin]', "peak-to-peak[bin]"]
 
     statistical_analysis_path = path + "\\statistical_results"
-    try:                        # This lines try to create a new directory 
+    try:                                                                                                                   # This lines try to create a new directory 
         os.mkdir(statistical_analysis_path)
     except OSError:
         print ("Creation of the directory %s failed" % statistical_analysis_path)
@@ -360,30 +360,30 @@ def configuration(csv_path):
   
     for item in name_list:
         if item == "24bit":
-            with open(statistical_analysis_path + '\\' + item + '.csv', "w") as csv_file: # Write columns into csv file
+            with open(statistical_analysis_path + '\\' + item + '.csv', "w") as csv_file:                                   # Write columns into csv file
                 csv_writer = csv.DictWriter(csv_file, fieldnames = features2)
                 csv_writer.writeheader()
         else:
-            with open(statistical_analysis_path + '\\' + item + '.csv', "w") as csv_file: # Write columns into csv file
+            with open(statistical_analysis_path + '\\' + item + '.csv', "w") as csv_file:                                   # Write columns into csv file
                 csv_writer = csv.DictWriter(csv_file, fieldnames = features1)
                 csv_writer.writeheader()
             
-    # Write datas into csv file
-    df4 = pd.DataFrame(dict_results[0], columns = features1) # Range4
+                                                                                                                            # Write datas into csv file
+    df4 = pd.DataFrame(dict_results[0], columns = features1)                                                                # Range4
     df3 = pd.DataFrame(dict_results[1], columns = features1)
     df2 = pd.DataFrame(dict_results[2], columns = features1)
     df1 = pd.DataFrame(dict_results[3], columns = features1)
-    df0 = pd.DataFrame(dict_results[4], columns = features2) # 24 Bit results
+    df0 = pd.DataFrame(dict_results[4], columns = features2)                                                                # 24 Bit results
     dataFrame_list = [df4, df3, df2, df1, df0]
 
-    # Line plots
+                                                                                                                            # Line plots
     step_by_step_plot(path, range1, "RANGE1[V]", step_num)
     step_by_step_plot(path, range2, "RANGE2[V]", step_num)
     step_by_step_plot(path, range3, "RANGE3[V]", step_num)
     step_by_step_plot(path, range4, "RANGE4[V]", step_num)
     step_by_step_plot(path, Adc_24bit, "24bit[bin]", step_num)
 
-    # FFT plots
+                                                                                                                            # FFT plots
     fft_of_ranges_with_zero_current(path, range1, "RANGE1[V]")
     fft_of_ranges_with_zero_current(path, range2, "RANGE2[V]")
     fft_of_ranges_with_zero_current(path, range3, "RANGE3[V]")
@@ -391,7 +391,7 @@ def configuration(csv_path):
     fft_of_ranges_with_zero_current(path, Adc_24bit, "24bit[bin]")    
 
     i = 0
-    for frame in dataFrame_list:    # This for loop fills csv files 
+    for frame in dataFrame_list:                                                                                            # This for loop fills csv files 
         frame.to_csv (statistical_analysis_path + '\\' + name_list[i] + '.csv', index = False, header = True)
         i += 1
 
